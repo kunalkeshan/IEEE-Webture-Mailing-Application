88a735f4d8bd15f9bae54555401afd666bf8fd04
kunalkeshan <kunalkeshan12@gmail.com>
2022-09-19 18:39:33 +0530

    form link and sheet keys updated

--- config/index.js
+++ config/index.js
@@ -9,24 +9,24 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const sheets = {
     ALL_PARTICIPANTS_SPREADSHEET_PUBLISHED_URL: isProduction ?
-        'https://docs.google.com/spreadsheets/d/e/2PACX-1vSApO_kbepcQ2UBwUmqoyWaf1Wmx25VVsgNkOlRZTuuYyK2bO9OZ2B7zOzwgmm8TwePQkzg0p3F4iXd/pub?gid=1039896981&single=true&output=csv'
+        'https://docs.google.com/spreadsheets/d/e/2PACX-1vT78kCQKYBM1ISFr2CJY5A4L-EJK0NQq74FOnLcCgpA491-oIAbNsVL1O2dhFuvDXjniQhzsFkV0Vf4/pub?gid=327840845&single=true&output=csv'
         : '',
 }
 
 const DB_URL = isProduction ? process.env.DB_URL_PROD : process.env.DB_URL_TEST;
 
 const SHEET_KEYS = {
-    EMAIL: "What's your SRMIST email address?",
-    PHONE: "What's your phone number?",
+    EMAIL: "Team Lead's SRMIST Email ID?",
+    PHONE: "Team Lead's Contact Number:",
     REGISTER_NO: "What's your Register Number?",
     TOKEN: "Token",
-    NAME: "What's your full name?",
+    NAME: "First off, what'd we call your team?",
     SUBMITTED_AT: "Submitted At",
     PAID: "Paid",
 }
 
 const SHEET_KEYS_PROD = {
-    EMAIL: 'Your SRMIST Email ID?',
+    EMAIL: 'Team Lead\'s SRMIST Email ID?This question is required.',
     PHONE: 'Your Contact Number:',
     REGISTER_NO: 'What\'s your SRM Register Number?',
     TOKEN: 'Token',
